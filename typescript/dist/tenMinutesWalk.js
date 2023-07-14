f386d7bcf25b7c50a21ce62433888afdf8f733b7
Krasyo <doanusho@gmail.com>
2023-07-14 21:28:07 +0100

    ten minute walk kata

---
+++ typescript/dist/tenMinutesWalk.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isValidWalk = void 0;
+function isValidWalk(walk) {
+    if (walk.length !== 10)
+        return false;
+    const directions = {
+        n: 1,
+        s: -1,
+        e: 100,
+        w: -100,
+    };
+    let finalN = 0;
+    for (let direction of walk) {
+        finalN += directions[direction];
+    }
+    return finalN === 0;
+}
+exports.isValidWalk = isValidWalk;
+console.log(isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]));
+console.log(isValidWalk(["s", "s", "n", "s", "n", "s", "n", "s", "n", "s"]));
+/*
+You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment,
+so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones --
+everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']).
+You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block,
+so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!)
+and will, of course, return you to your starting point. Return false otherwise.
+*/
