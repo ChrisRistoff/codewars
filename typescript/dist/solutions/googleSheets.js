a02f1c228687cbb825d6db6deef2ac5d717a53f5
Krasyo <doanusho@gmail.com>
2023-07-29 20:29:33 +0100

    google sheet addresses kata

---
+++ typescript/dist/solutions/googleSheets.js
@@ -0,0 +1,17 @@
+function getCellAdresses(range) {
+    const [start, end] = range.split(":");
+    const startCol = start[0];
+    const startRow = Number(start.slice(1));
+    const endCol = end[0];
+    const endRow = Number(end.slice(1));
+    const cellAddresses = [];
+    for (let row = startRow; row <= endRow; row++) {
+        for (let col = startCol.charCodeAt(0); col <= endCol.charCodeAt(0); col++) {
+            cellAddresses.push(String.fromCharCode(col) + row);
+        }
+    }
+    return cellAddresses;
+}
+console.log(getCellAdresses("A1:A10")); // ["A1", "A2", "B1", "B2"]
+console.log(getCellAdresses("A1:A1")); // []
+console.log(getCellAdresses("A1:Z9")); // ["A1", "A2", ..., "Z9"]
---
+++ typescript/solutions/googleSheets.ts
@@ -0,0 +1,27 @@
+function getCellAdresses(range: string): string[] {
+
+  const [start, end] = range.split(":");
+
+  if (start === end) {
+    return [];
+  }
+
+  const startCol = start[0];
+  const startRow = Number(start.slice(1));
+  const endCol = end[0];
+  const endRow = Number(end.slice(1));
+
+  const cellAddresses = [];
+
+  for (let row = startRow; row <= endRow; row++) {
+    for (let col = startCol.charCodeAt(0); col <= endCol.charCodeAt(0); col++) {
+      cellAddresses.push(String.fromCharCode(col) + row);
+    }
+  }
+
+  return cellAddresses;
+}
+
+console.log(getCellAdresses("A1:A10")); // ["A1", "A2", "B1", "B2"]
+console.log(getCellAdresses("A1:A1")); // []
+console.log(getCellAdresses("A1:Z9")); // ["A1", "A2", ..., "Z9"]
