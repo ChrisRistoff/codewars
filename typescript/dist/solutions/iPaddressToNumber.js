e728b32f1d6220e0924f9cce67e393d936e0af9d
Krasyo <doanusho@gmail.com>
2023-08-01 18:56:22 +0100

    number to IP still not working correct for some of the tests

--- typescript/dist/solutions/iPaddressToNumber.js
+++ typescript/dist/solutions/iPaddressToNumber.js
@@ -20,23 +20,35 @@ function ipToNum(ip) {
     return parseInt(binary, 2);
 }
 exports.ipToNum = ipToNum;
+function decimalToBinary(x) {
+    let binary = "";
+    while (x > 0) {
+        binary = (x % 2) + binary;
+        x = Math.floor(x / 2);
+    }
+    return binary;
+}
 function numToIp(x) {
-    let binary = x.toString(2);
-    let binaryArray = [128, 64, 32, 16, 8, 4, 2, 1];
+    const binary = decimalToBinary(x);
+    const binaryArray = binary.split("");
+    // Pad the binary string with leading zeros to make it a multiple of 8 bits
+    while (binaryArray.length % 8 !== 0) {
+        binaryArray.unshift("0");
+    }
+    const binaryArrayLength = binaryArray.length;
     let ip = "";
-    for (let i = 0; i < binary.length; i += 8) {
+    for (let i = 0; i < binaryArrayLength; i += 8) {
+        let binarySlice = binaryArray.slice(i, i + 8);
+        let binarySliceLength = binarySlice.length;
         let num = 0;
-        for (let j = 0; j < 8; j++) {
-            if (binary[i + j] === "1") {
-                num += binaryArray[j];
+        for (let j = 0; j < binarySliceLength; j++) {
+            if (binarySlice[j] === "1") {
+                num += Math.pow(2, 7 - j);
             }
         }
-        ip += num.toString();
-        if (i < binary.length - 8) {
-            ip += ".";
-        }
+        ip += num + ".";
     }
-    return ip;
+    return ip.slice(0, -1);
 }
 exports.numToIp = numToIp;
 console.log(ipToNum("192.168.1.1"));
--- typescript/solutions/iPaddressToNumber.ts
+++ typescript/solutions/iPaddressToNumber.ts
@@ -20,26 +20,47 @@ export function ipToNum(ip: string): number {
   return parseInt(binary, 2)
 }
 
-export function numToIp(x: number): string {
-  let binary = x.toString(2)
-  let binaryArray = [128, 64, 32, 16, 8, 4, 2, 1]
-  let ip = ""
+function decimalToBinary(x: number): string {
+  let binary = ""
 
-  for (let i = 0; i < binary.length; i += 8) {
-    let num = 0
-    for (let j = 0; j < 8; j++) {
-      if (binary[i + j] === "1") {
-        num += binaryArray[j]
-      }
-    }
-    ip += num.toString()
-    if (i < binary.length - 8) {
-      ip += "."
-    }
+  while (x > 0) {
+    binary = (x % 2) + binary
+    x = Math.floor(x / 2)
   }
 
-  return ip
+  return binary
 }
+
+export function numToIp(x: number): string {
+  const binary = decimalToBinary(x);
+  const binaryArray = binary.split("");
+
+  // Pad the binary string with leading zeros to make it a multiple of 8 bits
+  while (binaryArray.length % 8 !== 0) {
+    binaryArray.unshift("0");
+  }
+
+  const binaryArrayLength = binaryArray.length;
+  let ip = "";
+
+  for (let i = 0; i < binaryArrayLength; i += 8) {
+    let binarySlice = binaryArray.slice(i, i + 8);
+    let binarySliceLength = binarySlice.length;
+    let num = 0;
+
+    for (let j = 0; j < binarySliceLength; j++) {
+      if (binarySlice[j] === "1") {
+        num += Math.pow(2, 7 - j);
+      }
+    }
+
+    ip += num + ".";
+  }
+
+  return ip.slice(0, -1);
+}
+
 
 console.log(ipToNum("192.168.1.1"))
 console.log(numToIp(3232235777))
+console.log(numToIp(167772160))
